7ab7f6ef07cd547bdc4ba7d4336630eda45b7a9e
Yue Du <duyuescience@gmail.com>
2022-02-11 15:22:52 -0500

    'complete'

--- Initiation_Initiation.js
+++ Initiation_Initiation.js
@@ -221,17 +221,17 @@ var resp_ind = [];
 var symb_map_ind_shuffle;
 ////////////////////////////////////
 var tr_block_hand = 4;
-var num_trials_hand = 4;
+var num_trials_hand = 96;
 var num_trials_cr = 2000;
-var num_criterion = 1;
+var num_criterion = 5;
 
 var tr_block_old = 0;
 var tr_block_new_swap = 0;
-var tr_block_new_stop = 2;
+var tr_block_new_stop = 6;
 
-var prac_old_block = 1;   // set of criterion + practice (rt_blocks)
+var prac_old_block = 12;   // set of criterion + practice (rt_blocks)
 var rt_block = 1;
-var num_trials = 10;
+var num_trials = 96;
 
 var tr_hand_yes = 1;
 var rt_hand_yes = 1;
